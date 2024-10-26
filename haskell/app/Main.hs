983f5f28f2b9af040800cc949c4e75d736b13e8b
astynax <astynax@users.noreply.github.com>
2024-10-26 20:42:19 +0400

    add the haskell version

---
+++ haskell/app/Main.hs
@@ -0,0 +1,24 @@
+{- cabal:
+build-depends:
+  base,
+  optparse-applicative,
+  text,
+  shelly
+-}
+
+{-# LANGUAGE Ghc2024 #-}
+
+module Main where
+
+import Options.Applicative
+import Shelly
+
+wordP :: Parser String
+wordP = strArgument $ metavar "PREFIX"
+
+main :: IO ()
+main = do
+  word <- execParser (info wordP mempty)
+  shelly $
+    cmd "grep" ("^" <> word) "/usr/share/dict/words"
+      -|- cmd "wc" "-l"
