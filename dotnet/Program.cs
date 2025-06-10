12888b2360bd7be0cfb63a0221212b90f1c1efc2
astynax <astynax@users.noreply.github.com>
2025-06-10 18:57:21 +0400

    add the dotnet/C# version

---
+++ dotnet/Program.cs
@@ -0,0 +1,18 @@
+﻿using CliWrap;
+
+await (args switch
+{
+    [var prefix] => count(prefix),
+    _ => Console.Error.WriteLineAsync("Usage: cw <PREFIX>")
+});
+
+async Task count(string prefix)
+{
+    await (
+    Cli.Wrap("grep").WithArguments(
+    ["^" + prefix, "/usr/share/dict/words"]
+         )
+     | Cli.Wrap("wc").WithArguments(["-l"])
+     | (Console.WriteLine, Console.Error.WriteLine)
+    ).ExecuteAsync();
+};
